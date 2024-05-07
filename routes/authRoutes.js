529c840888491dedcf1a69c4f9774f3155a3a26d
Josip Milan <josip.milan@aol.com>
2024-05-07 17:39:53 +0200

    Profile CRUD

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const authenticateToken = require("../middleware/authenticateToken");
 const User = require("../models/User");
 
 const secretKey = process.env.JWT_SECRET;
 
+// POST new user
 router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   try {
@@ -24,6 +26,7 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+// POST login user
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -56,6 +59,52 @@ router.post("/login", async (req, res) => {
   }
 });
 
+// GET the current user's profile
+router.get("/me", authenticateToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// PUT update the current user's profile
+router.put("/me", authenticateToken, async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.findById(req.user.userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Update fields if they are provided
+    if (username) user.username = username;
+    if (email) user.email = email;
+    if (password) {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+    }
+
+    await user.save();
+    res.status(200).json({
+      message: "Profile updated successfully",
+      data: { username: user.username, email: user.email },
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      res.status(400).json({ error: "Username or email already exists" });
+    } else {
+      res.status(500).json({ error: error.message });
+    }
+  }
+});
+
+// POST logout user
 router.post("/logout", (req, res) => {
   res.json({ message: "Logged out successfully" });
 });
